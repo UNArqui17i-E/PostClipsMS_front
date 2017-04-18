7bc354e83779fe11cec1c64a867509a8bd7849cc
Sebastian Moreno <sdmorenob@unal.edu.co>
2017-04-18 03:07:59 -0500

    Merge1

--- src/app/postclip/postclip-new/postclip-new.component.ts
+++ src/app/postclip/postclip-new/postclip-new.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Headers } from '@angular/http';
 import { PostclipService } from '../postclip.service';
 import { Postclip } from '../postclip';
 import { BoardService } from '../../board/board.service';
 import { Board } from '../../board/board';
-import { ActivatedRoute, Params, Router} from '@angular/router';
-import {Http, Response, Headers, RequestOptions} from '@angular/http';
-import { AuthenticationService } from '../../_services/index';
 // import { LocalStorageService } from 'angular-2-local-storage';
 
 @Component({
@@ -20,57 +18,51 @@ export class PostclipNewComponent implements OnInit {
   errorMessage: string;
   postclip = new Postclip;
   submitted: boolean = false;
-  routeId : number;
-  id: number;
 
   constructor(
-    private route: ActivatedRoute,
     private boardService: BoardService,
-    private http: Http,
-    private router: Router,
-    private authenticationService: AuthenticationService,
-    private postclipService: PostclipService//,
-
-    // private localStorage: LocalStorageService
+    private postclipService: PostclipService
   ){}
 
   ngOnInit( ){
-    //let timer = Observable.timer(0,5000);
-    //timer.subscribe(()=> this.boardService.getBoardsByUser( 1 ));//localStorage.getItem('id') ));
-    this.authenticationService.validate( localStorage.getItem('token') )
-    .subscribe(
-      data2 => {
-          alert(data2.valido);
-          if (data2.valido == "false" ){
-
-            this.router.navigate(['\login']);
-
-          }
-
-
-      });
+    // let timer = Observable.timer(0,5000);
+    // var id = +localStorage.getItem("id");
+    // timer.subscribe(
+    //   ()=> this.boardService.getBoardsByUser( id )
+    //          .subscribe(
+    //             data => {
+    //                 this.boards = <Board[]>data;
+    //             }
+    //          )
+    // );
   }
-
-  @Input()
-  board:Board;
 
   createPostclip(postclip : Postclip){
-      this.submitted = true
-      alert(localStorage.getItem("board_id"));
-      postclip.board_id = parseInt(localStorage.getItem("board_id"));
+    this.submitted = true
+    // alert( typeof(postclip.name) === "undefined" );
+    // alert( typeof(postclip.description) === "undefined" );
+    // alert( typeof(postclip.contentLink) === "undefined" );
+    // postclip.board_id = 1;
+    if( typeof(postclip.name) === "undefined" || typeof(postclip.description) === "undefined"
+        || typeof(postclip.description) === "undefined" || typeof(postclip.contentLink) === "undefined"
+        || typeof(postclip.board_id) === "undefined"){
+      (<HTMLInputElement>document.getElementById("alert")).setAttribute( "style", "display:block!important" );
+      alert( "You have one o more empty fields" );
+    }else{
+      postclip.board_id = +localStorage.getItem("board_id");
       this.postclipService.createPostclip( postclip )
-      .subscribe( data => {return true},
-        error => this.errorMessage= <any>error
-       )
-     }
-
-
-getBoards( id: number ){
-    this.boardService.getBoardsByUser( 1 )
-        .subscribe(
-          boards => this.boards = boards,
-          error => this.errorMessage= <any>error
-        )
+      .subscribe(
+        data => {
+          var str = String(data);
+          var n = str.search("201");
+          if( n != -1 ){
+            (<HTMLInputElement>document.getElementById("sumb")).setAttribute( "style", "display:block!important" );
+          }
+        }, error =>{
+          console.log("Error saving postclip");
+          return Observable.throw(error);
+        }
+      );
+    }
   }
-
 }
